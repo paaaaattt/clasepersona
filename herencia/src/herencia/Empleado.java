9ff372c1ab40112687ecf7c9c02891ac5cfb2402
paaaaattt <145470405+paaaaattt@users.noreply.github.com>
2023-11-28 09:01:06 -0600

    Add files via upload

---
+++ herencia/src/herencia/Empleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herencia;
+
+/**
+ *
+ * @author User
+ */
+class Empleado extends Persona {
+    double sueldo;
+    
+    
+    public void mostrarSueldo(){
+        System.out.println("Sueldo: "+ sueldo);
+    }
+
+        
+}
---
+++ herencia/src/herencia/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herencia;
+
+/**
+ *
+ * @author User
+ */
+class Persona {
+    String nombre;
+    int edad;
+    String sexo;
+    
+    public void mostrarDatos(){
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Sexo: "+ sexo);
+    }
+    
+}
